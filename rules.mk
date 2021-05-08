f10323bac0844fdc4c03a6d65c118682f89978a5
firoh860 <firohh.ass@gmail.com>
2021-05-08 05:15:45 +0200

    custom keymap

---
+++ rules.mk
@@ -0,0 +1,5 @@
+OLED_DRIVER_ENABLE  = yes
+EXTRAKEY_ENABLE = yes        # Audio control and System control(+450)
+NKRO_ENABLE = no            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight
+LTO_ENABLE      = yes
